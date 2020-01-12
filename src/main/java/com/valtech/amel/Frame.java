853cdde2e6bc0e9c53f2a6e69af15c51d6b4b776
amelhadjarbi <amel.arbi@valtech.com>
2020-01-12 09:24:08 +0100

    first Commit

---
+++ src/main/java/com/valtech/amel/Frame.java
@@ -0,0 +1,53 @@
+package com.valtech.amel;
+
+import java.util.ArrayList;
+        import java.util.List;
+
+public class Frame {
+
+    int nummer;
+    List<Integer> throwList = new ArrayList<>();// statt throw1 und throw2 ich habe eine Liste für throws
+    // erstellt
+
+    public Frame(int num) {
+        nummer = num;
+    }
+
+    @Override
+    public String toString() {
+        if (nummer <9) {
+            return "Frame " + nummer + " [first Throw =" + throwList.get(0) + ", second Throw =" + throwList.get(1)
+                    + ", score=" + getScore() + "]";
+
+        } else if (isStrike()){
+            return "Frame " + nummer + " [first Throw =" + throwList.get(0) + ", second Throw =" + throwList.get(1)
+                    + ", third Throw =" + throwList.get(2) + ", fourth Throw =" + throwList.get(3) + ", score="
+                    + getScore() + "]";
+        }else if(isSpare()) {
+            return "Frame " + nummer + " [first Throw =" + throwList.get(0) + ", second Throw =" + throwList.get(1)
+                    + ", third Throw =" + throwList.get(2) + ", score="
+                    + getScore() + "]";
+
+        }else {
+            return "Frame " + nummer + " [first Throw =" + throwList.get(0) + ", second Throw =" + throwList.get(1)
+                    + ", score=" + getScore() + "]";
+
+        }
+    }
+
+    public int getScore() {
+        return throwList.stream().mapToInt(i -> i).sum();
+    }
+
+    boolean isStrike() {
+        return (throwList.get(0) == 10);
+    }
+
+    boolean isSpare() {
+        if (!isStrike()) {
+            return ((throwList.get(0) + throwList.get(1)) == 10);
+        } else {
+            return false;
+        }
+    }
+}
