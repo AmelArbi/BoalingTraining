bd356430e6c6d57607cb0e38d55fb7078f5a7dfa
amelhadjarbi <amel.arbi@valtech.com>
2020-03-03 17:20:42 +0100

    add Mock Annotation, createPlayerGameThrowExceptionWhenGameNotInitialized test

--- src/test/java/com/valtech/amel/controller/PlayerGameRepositoryClassTests.java
+++ src/test/java/com/valtech/amel/controller/PlayerGameRepositoryClassTests.java
@@ -5,50 +5,63 @@ import com.valtech.amel.model.Game;
 import com.valtech.amel.model.PlayerGame;
 import com.valtech.amel.service.GameRepository;
 import com.valtech.amel.service.GameRepositoryClass;
-import com.valtech.amel.service.GameService;
 import com.valtech.amel.service.PlayerGameRepository;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PlayerGameRepositoryClassTests {
 
-    private GameRepositoryClass gameRepositoryClass;
-    private GameController gameController;
 
+
+    @Mock
     private GameRepository gameRepository;
+    @Mock
     private PlayerGameRepository playerGameRepository;
 
-    @Before// with BeforeClass, will be excecuted only one time before
-    public void init() {//with static, int() will be only one time excecuted
-        gameRepository = Mockito.mock(GameRepository.class);
-        playerGameRepository = Mockito.mock(PlayerGameRepository.class);
-        gameRepositoryClass = new GameRepositoryClass(gameRepository, playerGameRepository);
-        gameController = new GameController(new GameService(), gameRepositoryClass);
-
-    }
+    @InjectMocks
+    private GameRepositoryClass gameRepositoryClass;
 
     @Test
     public void GameCreated() {
+        when(gameRepository.save(any()))
+                .thenReturn(new Game(10));
         assertThat(gameRepositoryClass.createGame() > 0, is(true));
     }
 
+    @Test(expected = GameNotInitialized.class)
+    public void createPlayerGameThrowExceptionWhenGameNotInitialized() {
+        gameRepositoryClass.createPlayerGame(10);
+    }
+
+//    @Test
+//    private void createPlayerGameTest(){
+//       Game game = new Game(10);
+//
+//       when(gameRepository.findById(10l))
+//               .thenReturn(Optional.of(game));
+//
+//    }
+
     @Test
     public void getGameTest() {
-        when(gameRepository.save(any()))
-                .thenReturn(new Game(10));
-        when(gameRepository.findById(10l))
-                .thenReturn(Optional.of((new Game(10))));
+        Game game = new Game(10);
 
+        when(gameRepository.save(any()))
+                .thenReturn(game);
+        when(gameRepository.findById(10l))
+                .thenReturn(Optional.of((game)));
         when(playerGameRepository.save(any()))
                 .thenReturn(new PlayerGame(1));
-
-        when(playerGameRepository.findByGameAndById(gameRepository.findById(10l).get(), 1l))
+        when(playerGameRepository.findByGameAndById(game, 1l))
                 .thenReturn(Optional.of(new PlayerGame(1)));
 
         long gameId = gameRepositoryClass.createGame();
@@ -64,8 +77,13 @@ public class PlayerGameRepositoryClassTests {
 
     @Test(expected = PlayerNotFound.class)
     public void willReturnExceptionIfPlayerNotFound() {
-        long gameId = gameRepositoryClass.createGame();
-        gameRepositoryClass.getGame(gameId, 456);
+        Game game = new Game(10);
+
+        when(gameRepository.findById(10l))
+                .thenReturn(Optional.of(game));
+
+        gameRepositoryClass.getGame(10, 456);
     }
+
 
 }
